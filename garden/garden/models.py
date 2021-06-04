5c8cb15b8840d046ce8d727a3c8aca2eeaf3c0fe
Rebecca Conley <rebecca@momentumlearn.com>
2021-06-04 16:48:51 -0400

    update zones, note script for creating them

--- garden/garden/models.py
+++ garden/garden/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 from localflavor.us.models import USPostalCodeField
 
 
-
-
 class Profile(models.Model):
     PIEDMONT = 'PM'
     MOUNTAINS = 'MT'
@@ -37,7 +35,20 @@ class Plant(models.Model):
 
 
 class GrowingZone(models.Model):
-    name = models.CharField(max_length=100)
+    A = 'A'
+    B = 'B'
+    
+    ZONE_CHOICES = [
+        (A, 'a'),
+        (B, 'b')
+    ]
+    number = models.IntegerField()
+    subdivision = models.CharField(max_length=1, choices=ZONE_CHOICES)
+
+# python script for making growing Zones in US
+# for i in range(14):
+# ...     for sub in ['A', 'B']:
+# ...             GrowingZone.objects.create(number=i, subdivision=sub)
     
     def __str__(self):
-        return self.name
+        return f'{self.number} {self.subdivision}'
